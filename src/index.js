289d29e0ffcf81edbe9be9cbccc9348a7c1636d4
fireworks99 <2639237361@qq.com>
2026-07-14 18:18:38 +0800

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,90 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const corsHeaders = {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+	"Access-Control-Allow-Headers": "Content-Type, Authorization",
+	"Access-Control-Expose-Headers": "*",
+};
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
+
+		// 处理预检请求
+		if (request.method === "OPTIONS") {
+			return new Response(null, {
+				headers: corsHeaders,
+			});
+		}
+
+		if (request.method === "POST") {
+
+			const url = new URL(request.url);
+
+			if (url.pathname === "/chat") {
+				return chat(request, env);
+			}
+
+			return new Response("Not Found", {
+				status: 404,
+				headers: corsHeaders
+			});
+
+		}
+
+		return new Response("Not Found", {
+			status: 404,
+			headers: corsHeaders,
+		});
+
 	},
 };
+
+async function chat(request, env) {
+	try {
+		const body = await request.json();
+
+		const contents = body.messages.map(msg => ({
+			role: msg.role === "assistant" ? "model" : "user",
+			parts: [
+				{
+					text: msg.content,
+				},
+			],
+		}));
+
+		const response = await fetch(
+			"https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:streamGenerateContent?alt=sse",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"X-goog-api-key": env.GEMINI_API_KEY,
+				},
+				body: JSON.stringify({
+					contents
+				}),
+			}
+		);
+
+		if (!response.ok) {
+			return new Response(await response.text(), {
+				status: response.status,
+				headers: corsHeaders,
+			});
+		}
+
+		return new Response(response.body, {
+			status: response.status,
+			headers: {
+				...corsHeaders,
+				"Content-Type": "text/event-stream",
+				"Cache-Control": "no-cache",
+				"Connection": "keep-alive",
+			},
+		});
+	} catch (e) {
+		return new Response(e.message, {
+			status: 500,
+			headers: corsHeaders,
+		});
+	}
+}
